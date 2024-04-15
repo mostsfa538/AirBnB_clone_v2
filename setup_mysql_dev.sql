ebecc42a5b56000e5f9ff8e13f7d81cbce76e6f8
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-15 19:43:10 +0200

    done

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,7 @@
 CREATE database IF NOT EXISTS hbnb_dev_db;
-drop user hbnb_dev@localhost;
-flush privileges;
-CREATE user hbnb_dev@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON database.hbnb_dev_db TO hbnb_dev@'localhost';
+-- drop user hbnb_dev@localhost;
+-- flush privileges;
+CREATE user IF NOT EXISTS hbnb_dev@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@'localhost';
+GRANT SELECT ON performance_schema.* TO hbnb_dev@'localhost';
+FLUSH PRIVILEGES;
