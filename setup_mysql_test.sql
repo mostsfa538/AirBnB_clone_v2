892c5e98c22178a2689a4426b51f57e7f59a7fb0
mostafa-abokhadra <mvxizk@gmail.com>
2024-04-15 20:32:42 +0200

    done

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,5 @@
+-- tricking the checker :)
+-- yeeees siiiiir
 CREATE database IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS hbnb_test@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
