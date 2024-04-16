e9d21fc66f313709aedb004466fd5c83d998f65d
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-16 12:06:51 +0200

    check

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,15 +7,17 @@ import os
 
 load_dotenv()
 
+
 class DBStorage:
     __engine = None
     __session = None
+
     def __init__(self):
         try:
             self.__engine = sqlalchemy.create_engine(
-                    'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                'mysql+mysqldb://{}:{}@localhost/{}'.format(
                     'hbnb_dev', 'hbnb_dev_pwd', 'hbnb_dev_db'),
-                    pool_pre_ping=True)
+                pool_pre_ping=True)
             Session = sessionmaker(bind=self.__engine)
             session = Session()
         except Exception:
@@ -25,9 +27,3 @@ class DBStorage:
         if os.environ['HBNB_ENV'] == 'test':
             for tbl in reversed(meta.sorted_tables):
                 self.__engine.execute(tbl.delete())
-
-
-
-
-
-
