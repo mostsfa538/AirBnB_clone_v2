715bf0ac3ce21b1c7fb9892aaeb78175e9faa3de
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-17 09:28:03 +0200

    update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import models
 
 
 class Amenity(BaseModel):
-    name = ""
+    """ """
+    __tablename__ = "amenities"
+
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary='place_amenity')
