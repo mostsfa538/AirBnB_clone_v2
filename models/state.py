7a62e261652c1c830c89f4cb8ef3ff083c241fd8
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-17 09:25:02 +0200

    update state

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="deleted")
+    cities = relationship("City", backref="state", cascade="delete")
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         @property
