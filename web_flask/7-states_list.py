e8e659d3405de04141426670d2a37a5586c59960
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-16 17:19:36 +0300

    states_list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """ Here goes every thing """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 @app.route('/states_list', states_list=False)
 def list_tables():
     """Display a list of all State objects."""
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
 
