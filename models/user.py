5208055de02ddf42201bfa97f7f3374f073c0ad3
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-17 09:46:08 +0300

    update

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
@@ -14,5 +15,5 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    reviews = relationship("Review", backref="user", cascade="delete")
     places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
