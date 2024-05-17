f93f9fa7918567d3b2520584e8390a3808773558
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-17 10:05:47 +0300

    state list

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,4 +68,4 @@ class DBStorage:
 
     def close(self):
         """close session"""
-        self.__session.remove()
+        self.__session.close()
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,7 +7,7 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', states_list=False)
+@app.route('/states_list', strict_slashes=False)
 def list_tables():
     """Display a list of all State objects."""
     states = storage.all(State).values()
