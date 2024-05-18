004844e3abd3e25c7530252b9ccc9fd3cad5ddc1
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-18 15:54:33 +0300

    states

---
+++ web_flask/9-states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" start point """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """ """
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """ """
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    for state in states:
+        if state.id == id:
+            return render_template("9-states.html", states=state)
+    return render_template("9-states.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
